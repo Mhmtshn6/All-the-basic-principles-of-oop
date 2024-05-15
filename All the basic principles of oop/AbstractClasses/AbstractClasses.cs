161c958f30fba8a5e6e9a9f8be680243b988ef39
Mhmtshn6 <mehmet68064@gmail.com>
2024-05-15 12:28:08 +0300

    All the basic principles of oop
    Example of all the basic principles of oop

---
+++ All the basic principles of oop/AbstractClasses/AbstractClasses.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    internal class AbstractClasses
+    {
+        static void Main(string[] args)
+        {
+            Information n1 = new Note();
+            Information n2 = new Data();
+
+            n1.InformationAdded();
+            n1.InformationDeleted();
+
+            n2.InformationAdded();
+            n2.InformationDeleted();
+
+            Console.ReadLine();
+        }
+    }
+    abstract class Information
+    {
+        public void InformationAdded()
+        {
+            Console.WriteLine("Succesful");
+        }
+        public abstract void InformationDeleted();
+    }
+    class Note : Information
+    {
+        public override void InformationDeleted()
+        {
+            Console.WriteLine("Note is Deleted");
+        }
+    }
+    class Data : Information
+    {
+        public override void InformationDeleted()
+        {
+            Console.WriteLine("Data is Deleted");
+        }
+    }
+}
---
+++ All the basic principles of oop/All the basic principles of oop/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace All_the_basic_principles_of_oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
